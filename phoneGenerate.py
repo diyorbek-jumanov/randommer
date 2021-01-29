c2bd4a4ec4dbb6dc0973903d01ca31730a9ff338
diyorbekjumanov <diyorbekjumanov1120@gmail.com>
2021-01-29 10:56:04 +0000

    request phone generate

---
+++ phoneGenerate.py
@@ -0,0 +1,15 @@
+import requests
+
+key = '2d794c6f46094ceb96bd719c1c26c984'
+url = 'https://randommer.io/api/Phone/Generate'
+
+h = {'X-Api-Key': key}
+query = {
+    'CountryCode ': 'uz',
+    'Quantity': 1
+}
+
+r = requests.get(url, params=query, headers=h)
+
+print(r.url)
+print(r.json())
